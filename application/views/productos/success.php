5951e581b2c41b3ec297de772443971a7f5e5cd4
Enrique Munguía <enrique7mc@gmail.com>
2017-01-21 12:46:09 -0600

    genera vista de éxito al crear producto

---
+++ application/views/productos/success.php
@@ -0,0 +1,5 @@
+<?php
+echo '<h1>El producto se agregó correctamente</h1>';
+
+echo '<p><a class="pure-button pure-button-primary" href="'.site_url('productos').'">Listado de productos</a></p>';
+echo '<p><a class="pure-button pure-button-primary" href="'.site_url('productos/create').'">Agregar nuevo</a></p>';
