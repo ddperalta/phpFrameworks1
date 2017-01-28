0b6c15bfcb05aabb047057ff44684fab02607015
YiroYi <yiroyi@live.com>
2017-01-28 13:56:42 -0300

    Nueva Rama nombre: yiro

---
+++ application/views/login.php
@@ -0,0 +1,12 @@
+<div><a>Login<a></div>
+
+<div>
+	<label>Usuario</label>
+	<input type="Text">
+
+	<label>Contraseña</label>
+	<input type="Text">
+
+	<button>Enviar</button>
+
+</div>
--- database.sql
+++ database.sql
@@ -1,6 +1,23 @@
 CREATE TABLE productos (
-	id INTEGER,
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
 	nombre TEXT,
 	descripcion TEXT,
 	precio REAL
+);
+
+CREATE TABLE usuarios(
+   id INTEGER  PRIMARY KEY AUTOINCREMENT,
+   login TEXT,
+   password TEXT
+);
+
+CREATE TABLE perfil(
+   id INTEGER  PRIMARY KEY  AUTOINCREMENT,
+   id_usuario INTEGER,
+   rol TEXT,
+   direccion TEXT,
+   nombre TEXT,
+   telefono INTEGER,
+
+   FOREIGN KEY(id_usuario) REFERENCES usuarios(id)
 );
